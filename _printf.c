3573cb43bf24ca562f6055a72f706871cc7301b2
Your Name <you@example.com>
2025-04-05 16:44:18 +0000

    T

---
+++ _printf.c
@@ -0,0 +1,68 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * _printf - Produces output according to a format
+ * @format: A string containing zero or more format specifiers
+ *
+ * Return: The number of characters printed (excluding the null byte)
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i = 0, count = 0;
+	char c;
+	char *str;
+
+	if (!format)
+		return (-1);
+
+	va_start(args, format);
+
+	while (format[i])
+	{
+		if (format[i] == '%' && format[i + 1])
+		{
+			i++;
+			if (format[i] == 'c')
+			{
+				c = va_arg(args, int);
+				write(1, &c, 1);
+				count++;
+			}
+			else if (format[i] == 's')
+			{
+				str = va_arg(args, char *);
+				if (!str)
+					str = "(null)";
+				while (*str)
+				{
+					write(1, str, 1);
+					str++;
+					count++;
+				}
+			}
+			else if (format[i] == '%')
+			{
+				write(1, "%", 1);
+				count++;
+			}
+			else
+			{
+				write(1, "%", 1);
+				write(1, &format[i], 1);
+				count += 2;
+			}
+		}
+		else
+		{
+			write(1, &format[i], 1);
+			count++;
+		}
+		i++;
+	}
+
+	va_end(args);
+	return (count);
+}
