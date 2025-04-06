4da2fc10cd69f550963ccd7058322ce50bf000d5
Your Name <you@example.com>
2025-04-06 04:29:58 +0000

    T

--- main.h
+++ main.h
@@ -9,6 +9,7 @@ int print_char(va_list args);
 int print_string(va_list args);
 int print_percent(void);
 int print_int(va_list args);
+int print_digits(unsigned int num);
 int print_unknown(char specifier);
 
 #endif /* MAIN_H */
